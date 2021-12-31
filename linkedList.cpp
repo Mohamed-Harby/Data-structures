16b54fb79a04ebe70d45124c246755a650705d3d
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2021-12-31 18:13:46 +0200

    Update linkedList.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -6,7 +7,11 @@ struct Node {
     int data{};
     Node *next{};
 
-    explicit Node(int d) : data(d) {} // 'explict' to avoid the implicit conversion 'int' and 'Node'
+    explicit Node(int d) : data(d) {} // 'explict' to avoid the implicit conversion between 'int' and 'Node'
+
+    ~Node() {
+        cout << "the node with value " << data << " with address " << this << " is destroyed\n";
+    }
 };
 
 class linkedList {
@@ -26,6 +31,14 @@ public:
     void delete_back();
 
     void delete_front();
+
+    void clear();
+
+    bool nullHead();
+
+    bool nullTail();
+
+    void verify();
 };
 
 void linkedList::print() {
@@ -34,6 +47,7 @@ void linkedList::print() {
         cout << curr->data << ' ';
         curr = curr->next;
     }
+    cout << '\n';
 }
 
 void linkedList::push_back(int data) {
@@ -59,15 +73,76 @@ void linkedList::push_front(int data) {
 }
 
 void linkedList::delete_back() {
-
+    // write your code here
+    --length;
 }
 
 void linkedList::delete_front() {
     Node *tmp = head;
-    head = head->next;
+    if (head != nullptr)
+        head = head->next;
+
+    if (tail == tmp)    // all items are deleted
+        tail = nullptr;
+
     delete tmp;
+    --length;
+}
+
+void linkedList::clear() {
+    Node *tmp;
+    while (head != nullptr) {
+        tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+    tail = nullptr;
+}
+
+bool linkedList::nullHead() {
+    return head == nullptr;
+}
+
+bool linkedList::nullTail() {
+    return tail == nullptr;
+}
+
+void linkedList::verify() {
+    int verified_length = 0;
+    Node *it = head;
+    while (it != nullptr) {
+        ++verified_length;
+        it = it->next;
+    }
+    delete it;
+    assert(verified_length == length);
+
+    if (verified_length == 0)
+        assert(head == nullptr && tail == nullptr);
+    else if (verified_length == 1)
+        assert(head == tail && head != nullptr && tail != nullptr);
+    else
+        assert(head != tail && head != nullptr && tail != nullptr);
+
+    assert(tail->next == nullptr);
 }
 
 int main() {
+    linkedList ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.delete_front();
+    ll.delete_front();
+    ll.delete_front();
+    ll.delete_front();
+    ll.push_back(5);
+    ll.push_back(9);
+//    ll.clear();
+
+    ll.verify();
+//    ll.delete_front();
+//    ll.print();
 
 }
